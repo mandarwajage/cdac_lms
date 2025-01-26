5fc18bc768a4dc90cf02a07fe49b6e914a749114
mandar wajage <mandarwajage@gmail.com>
2025-01-26 22:24:50 +0530

    added use model

--- server/models/user.modle.ts
+++ server/models/user.modle.ts
@@ -1,4 +1,62 @@
-import mongoose,{Document,Model,Schema} from "mongoose";
-import bcrypt from 'bcryptjs';
+import mongoose, { Document, Model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const useSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "PLease enter your Email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "Please enter a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "PLease enter your Email"],
+      minlegth: [6, "Password must be atleast 6 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamp: true }
+);
